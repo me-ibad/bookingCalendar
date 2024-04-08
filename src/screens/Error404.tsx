5cafdc65b90e18f139296117cfc90fab83bbc68d
Ibad Ur Rehman <ibadurrehman78@gmail.com>
2024-04-08 23:07:59 +0200

    error pages

---
+++ src/screens/Error404.tsx
@@ -0,0 +1,11 @@
+function Error404() {
+  return (
+    <div className='App' data-testid='dashboard'>
+      <div className='my-4'>
+        <div>Error404</div>
+      </div>
+    </div>
+  );
+}
+
+export default Error404;
