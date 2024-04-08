c22dbc5366f008c5acad41fb8b10849ddaedc7e1
Ibad Ur Rehman <ibadurrehman78@gmail.com>
2024-04-08 23:27:33 +0200

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import './App.css';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './screens/Dashboard';
 import BookingDetails from './screens/BookingDetails';
-import Error404 from './screens/Error404';
 function App(): JSX.Element {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/details' element={<BookingDetails />} />
-        <Route path='*' element={<Error404 />} />
       </Routes>
     </Router>
   );
